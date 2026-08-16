234c3c4d06cd5f0efd27bd3f9d9229292fd7d868
Fuad Tasin <fuadtasin@Fuads-MacBook-Air.local>
2026-08-17 00:05:00 +0600

    See the previous commit

--- prompts/dynamic_ui.py
+++ prompts/dynamic_ui.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate,load_prompt
 
 model=ChatGroq(
     model="openai/gpt-oss-120b",temperature=0
@@ -25,21 +25,7 @@ length_input=st.selectbox(
     ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"]
 )
 
-template=PromptTemplate(
-    input_variables=["paper_input","style_input","length_input"],
-    template=(
-        """Please summarize the research paper titled "{paper_input}" with the following specifications:
-        Explanation Style: {style_input}
-        Explanation Length: {length_input}
-        1. Mathematical Details:
-        - Include relevant mathematical equations if present in the paper.
-        - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-        2. Analogies:
-        - Use relatable analogies to simplify complex ideas.
-        If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-        Ensure the summary is clear, accurate, and aligned with the provided style and length."""
-    )
-)
+template=load_prompt('template.json')
 
 if st.button("sumarize"):
     prompts=template.invoke(
